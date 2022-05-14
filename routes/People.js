a3398ca68db963e9a1b53966fcd35104fadd5ea4
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-05-14 19:26:04 +0530

    Added swagger

--- routes/People.js
+++ routes/People.js
@@ -15,4 +15,48 @@ Router.get("/", (req, res) => {
   });
 });
 
+Router.post("/", (req, res) => {
+  const name = req.body.name;
+  const query = `INSERT INTO \`people\`(\`name\`) VALUES ("${name}")`;
+  mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.json({
+        data: rows,
+      });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+Router.delete("/", (req, res) => {
+  const name = req.body.name;
+  const query = `DELETE FROM \`people\` WHERE name = "${name}"`;
+  mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.json({
+        data: rows,
+      });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+Router.put("/", (req, res) => {
+  const newName = req.query.newName;
+  const oldName = req.body.oldName;
+
+  const query = `UPDATE \`people\` SET \`name\` = "${newName}" WHERE \`name\` = "${oldName}"`;
+  mySqlConnection.query(query, (err, rows, fields) => {
+    if (!err) {
+      res.json({
+        data: rows,
+      });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
 module.exports = Router;
--- server.js
+++ server.js
@@ -1,17 +1,20 @@
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger-output.json");
 const bodyParser = require("body-parser");
 const express = require("express");
 const mySqlConnection = require("./utils/connection");
-const PeopleRoutes = require("./routes/People");
+const Router = require("./routes");
 
 const app = express();
 app.use(bodyParser.json());
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 mySqlConnection.connect((err) => {
   if (!err) {
     console.log("Connected");
 
     app.listen(3000);
-    app.use("/people", PeopleRoutes);
+    app.use("/", Router);
   } else {
     console.log("Error while connecting!!");
   }
